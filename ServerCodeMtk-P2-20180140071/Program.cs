ac910946b3c58726907f7124061401f5fbccebf6
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-10-17 15:18:15 +0700

    Add project files.

---
+++ ServerCodeMtk-P2-20180140071/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCodeMtk_P2_20180140071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
